bdbc52cd1d41bdab91e7fbf35deb12b06dac3258
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-02 12:35:40 +0900

    style: login registration form

--- eap/app/login/page.tsx
+++ eap/app/login/page.tsx
@@ -12,43 +12,43 @@ export default function LoginPage() {
 
     return (
         <div className="login-page">
-            <form>
-                <div>
-                    <h1>Login</h1>
+            <form className="login-form">
+                <div className="login-panel">
+                    <h1 className="login-title">Login</h1>
                 </div>
                 {
                     isLoginMode ? (
                         <div>
-                            <div>
-                                <label>Username</label>
-                                <input type="text" />
+                            <div className="mb-4">
+                                <label className="input-label">Username</label>
+                                <input className="input-field" type="text" />
                             </div>
-                            <div>
-                                <label>Password</label>
-                                <input type="password" />
+                            <div className="mb-4">
+                                <label className="input-label">Password</label>
+                                <input className="input-field" type="password" />
                             </div>
-                            <button type="button">Login</button>
-                            <button type="button">Register new user</button>
+                            <button className="login-button" type="button">Login</button>
+                            <button className="move-to-register-button" type="button">Register new user</button>
                         </div>
                     ) : (
                         <div>
-                            <div>
-                                <label>Full Name</label>
-                                <input type="text" />
+                            <div className="mb-4">
+                                <label className="input-label">Full Name</label>
+                                <input className="input-field" type="text" />
                             </div>
                             <div>
-                                <label>Choose a username</label>
-                                <input type="text" />
+                                <label className="input-label">Choose a username</label>
+                                <input className="input-field" type="text" />
                             </div>
                             <div>
-                                <label>Set a password</label>
-                                <input type="password" />
+                                <label className="input-label">Set a password</label>
+                                <input className="input-field" type="password" />
                             </div>
                             <div>
-                                <label>Confirm password</label>
-                                <input type="password" />
+                                <label className="input-label">Confirm password</label>
+                                <input className="input-field" type="password" />
                             </div>
-                            <button type="button">Register</button>
+                            <button className="login-button" type="button">Register</button>
                         </div>
 
                     )
