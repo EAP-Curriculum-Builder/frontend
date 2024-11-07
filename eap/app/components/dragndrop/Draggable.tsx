be3728adc973daa4f50f553c26340743b82b9440
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-07 11:31:54 +0900

    build: draggable component

---
+++ eap/app/components/dragndrop/Draggable.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface DraggablesProps {
+    id: string;
+    onDragStart: (e:React.DragEvent<HTMLDivElement>, id:string) => void;
+    onDelete: () => void;
+    exerciseType: string;
+}
+
+const Draggable: React.FC<DraggablesProps> = ({ id, onDragStart, onDelete, exerciseType }) => {
+
+    return (
+        <div
+            id={id}
+            className="draggable"
+            draggable
+            onDragStart={(e) => onDragStart(e, id)}
+        >
+            <button className="delete-draggable-button" onClick={onDelete}>
+                X
+            </button>
+            <span className='exercise-type-label'>{exerciseType}</span>
+        </div>
+    )
+}
+
+export default Draggable;
